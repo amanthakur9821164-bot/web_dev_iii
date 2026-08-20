1cf32d568fce9cb0ac024353400c346c12bca1fc
Aman <amanthakur9821164@gmail.com>
2026-08-20 14:28:38 +0530

    read operations with id

--- classes/week_02/day_04/content_02/server.js
+++ classes/week_02/day_04/content_02/server.js
@@ -1,22 +1,35 @@
 const express = require("express");
-const app = express();
-
-const PORT = 3000
-
+const app=express();
+const PORT= 3000
 
 const students = [
-    { rollno : 1 , name : "Aman"} ,
-    { rollno : 2 , name : "hamaz"} ,
-    { rollno : 3 , name : "CP"} ,
-    { rollno : 4 , name : "vaseem"} ,
-];
+    {rollNo:1, name:"Ronaldo", section:"core-b"},
+    {rollNo:2, name:"Messi", section:"core-b"},
+    {rollNo:3, name:"Lamine", section:"core-b"},
+    {rollNo:4, name:"Pele", section:"core-b"},
+    {rollNo:5, name:"Mbappe", section:"core-b"},
+    
+]
 
 // Read Operation
-app.get("/students", (req,res) => {
+app.get("/students",(req,res)=>{
     res.json(students)
 })
 
+// Read operation with id
+app.get("/students/:rollNo",(req,res)=>{
+    const id=req.params.rollNo;
+    const student=students.find((student)=>student.rollNo===Number(id));
+    if(!student){
+        res.status(404).json({success:false, message:"student not  found"});
+    }
+    res.json({success:true,student});
+    res.json(student);
+})
 
-app.listen(3000, ()=> console.log("Server is running on PORT 3000"));
 
 
+
+
+
+app.listen(PORT, ()=>console.log("Server is running on port 3000"));
